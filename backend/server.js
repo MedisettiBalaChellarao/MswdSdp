24c899714a499f6cc13bd1487a7162d923395f0b
MEDISETTI BALA CHELLARAO <2300031585@kluniversity.in>
2025-05-09 19:23:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
+import express from "express";
+import cors from "cors";
 import app from "./app.js";
 import cloudinary from "cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// Enable CORS
+app.use(cors());
+
+// Cloudinary configuration
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  
   api_key: process.env.CLOUDINARY_API_KEY,        
   api_secret: process.env.CLOUDINARY_API_SECRET,  
 });
 
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
